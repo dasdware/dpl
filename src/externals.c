fd9d5f4b25f0ead7440ded87a5d0bae1a0aab75f
dasdware <info@dasdware.de>
2024-04-13 13:34:29 +0200

    #24 Added: External function dummy for printing strings

--- src/externals.c
+++ src/externals.c
@@ -14,17 +14,28 @@ DPL_ExternalFunction* dple_add_by_name(DPL_ExternalFunctions* externals, const c
     return &externals->items[index];
 }
 
-void _dple_print_callback(DPL_VirtualMachine* vm)
+void _dple_print_number_callback(DPL_VirtualMachine* vm)
 {
     printf("%f\n", dplv_peek(vm));
 }
 
+void _dple_print_string_callback(DPL_VirtualMachine* vm)
+{
+    //TODO: Support for printing strings
+    (void) vm;
+}
+
 void dple_init(DPL_ExternalFunctions* externals)
 {
-    DPL_ExternalFunction* print = dple_add_by_name(externals, "print");
-    nob_da_append(&print->argument_types, "number");
-    print->return_type = "number";
-    print->callback = _dple_print_callback;
+    DPL_ExternalFunction* print_number = dple_add_by_name(externals, "print");
+    nob_da_append(&print_number->argument_types, "number");
+    print_number->return_type = "number";
+    print_number->callback = _dple_print_number_callback;
+
+    DPL_ExternalFunction* print_string = dple_add_by_name(externals, "print");
+    nob_da_append(&print_string->argument_types, "string");
+    print_string->return_type = "string";
+    print_string->callback = _dple_print_string_callback;
 }
 
 void dple_free(DPL_ExternalFunctions *externals)
